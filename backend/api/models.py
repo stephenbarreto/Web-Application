7938567a9e85b7211156849ad7943caf73be2ad2
stephenbarreto <stephendias@hotmail.com.br>
2024-02-19 19:59:54 -0300

    fix regular expression

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,7 +8,7 @@ from django.utils.translation import gettext_lazy as _
 
 @deconstructible
 class UnicodeUsernameValidator(validators.RegexValidator):
-    regex = r"^[\w.@+-\s]+\Z"
+    regex = r"^[\w\s.@+-]+\Z"
     message = _(
         "Enter a valid username. This value may contain only letters, "
         "numbers, spaces, and @/./+/-/_ characters."
