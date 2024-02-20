9876d4334f859d013458489b2467330ae7e3b199
stephenbarreto <stephendias@hotmail.com.br>
2024-02-20 06:05:19 -0300

    update customer tickets view

---
+++ frontend/src/views/AdminCenter/components/TicketForm.vue
@@ -0,0 +1,54 @@
+<script>
+import MaterialInput from '@/components/MaterialInput.vue';
+
+export default {
+  name: 'TicketForm',
+  data() {
+    return {
+      formData: {
+        expire_date: '',
+        document: '',
+      },
+    }
+  },
+  props: {
+    signature: Object,
+  },
+  methods: {
+    getFormData() {
+      return this.formData;
+    },
+    setFile(e) {
+      this.formData.document = e.target.files[0];
+    },
+    formatDate({ value }) {
+      // Converte a data para o formato 'YYYY-MM-DD'
+      const date = new Date(value);
+      const formattedDate = date.toISOString().split('T')[0];
+      this.formData['expire_date'] = formattedDate;
+    }
+  },
+  computed: {
+    fileName() {
+      return this.formData.file ? this.formData.file.name : 'Nenhum arquivo selecionado';
+    },
+  },
+  components: {
+    MaterialInput
+  },
+  mounted() {
+    this.formData['signature'] = this.signature.id;
+  }
+}
+</script>
+
+<template>
+  <form action="">
+    <MaterialInput id="expire_date" class="input-group-outline my-3" name="expire_date"
+      :label="{ text: 'Data de validade', class: 'form-label' }" type="date" @input-change="formatDate" />
+    <div class="mt-3">
+      <label for="document">Selecione um boleto:</label>
+      <input type="file" id="document" ref="ContractInput" class="form-control" @change="setFile" />
+    </div>
+  </form>
+</template>
--- frontend/src/views/CustomerCenter/TicketsView.vue
+++ frontend/src/views/CustomerCenter/TicketsView.vue
@@ -3,8 +3,11 @@
 import NavBar from '@/components/Navbars/NavBar.vue'
 import SidebarNav from './components/SidebarNav.vue'
 import MaterialButton from '@/components/MaterialButton.vue'
+import ShowFile from './components/ShowFile.vue'
 
 import api from '@/services/api'
+import Swal from 'sweetalert2'
+import { createApp } from 'vue'
 
 export default {
   components: {
@@ -23,8 +26,63 @@ export default {
         this.tickets = response.data;
       }).catch(e => console.error('Erro ao obter os boletos: ', e))
     },
-    showTicket() {
-      alert('O boleto apareceria em um modal');
+    showTicket(ticket) {
+      // Criar um elemento div
+      const div = document.createElement('div');
+      // Montar o componente vue na div
+      const form = createApp(ShowFile, { file: ticket }).mount(div);
+
+      // Cria e exibe o modal sweet alert
+      return Swal.fire({
+        title: 'Boleto',
+        html: form.$el,
+        showCloseButton: true,
+        showConfirmButton: false,
+        customClass: {
+          popup: 'w-100 w-md-70 w-lg-60 w-xl-50',
+        },
+        preConfirm: () => {
+          // Retorna os dados do form do componente Vue
+          return form.getFormData();
+        },
+      })
+    },
+    getStatusName(status) {
+      let statusName = ''
+
+      switch (status) {
+        case 0:
+          statusName = 'Aguardando pagamento'
+          break;
+        case 1:
+          statusName = 'Pago'
+          break;
+        case 2:
+          statusName = 'Expirado'
+          break;
+      }
+      return statusName;
+    },
+    formatDate(dateString) {
+      // Verifica se a string de data está no formato 'YYYY-MM-DD'
+      if (!/^\d{4}-\d{2}-\d{2}$/.test(dateString)) {
+        console.error("Formato de data inválido. Use o formato 'YYYY-MM-DD'.");
+        return null;
+      }
+
+      // Divide a string da data em partes (ano, mês, dia)
+      const parts = dateString.split('-');
+      const year = parseInt(parts[0]);
+      const month = parseInt(parts[1]);
+      const day = parseInt(parts[2]);
+
+      // Cria um objeto Date com a data
+      const dateObject = new Date(year, month - 1, day);
+
+      // Formata a data para o padrão brasileiro ('DD/MM/YYYY')
+      const formattedDate = dateObject.toLocaleDateString('pt-BR');
+
+      return formattedDate;
     }
   },
   mounted() {
@@ -47,20 +105,21 @@ export default {
                   <thead>
                     <tr>
                       <th>Plano</th>
-                      <th>Data de emissão</th>
-                      <th>Data de vencimento</th>
-                      <th>Status</th>
+                      <th class="text-center">Data de emissão</th>
+                      <th class="text-center">Data de vencimento</th>
+                      <th class="text-center">Status</th>
                       <th></th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr v-for="(ticket, index) in tickets" :key="index">
-                      <td>{{ ticket.plan.name }}</td>
-                      <td>{{ ticket.created_date }}</td>
-                      <td>{{ ticket.expire_date }}</td>
-                      <td>{{ ticket.status }}</td>
-                      <td class="d-flex">
-                        <MaterialButton @click="showTicket" color="warning" margin="mb-0 mx-1" size="sm" icon="eye">
+                      <td>{{ ticket.signature.plan.name }}</td>
+                      <td class="text-center">{{ formatDate(ticket.created_date) }}</td>
+                      <td class="text-center">{{ formatDate(ticket.expire_date) }}</td>
+                      <td class="text-center">{{ getStatusName(ticket.status) }}</td>
+                      <td class="d-flex text-center">
+                        <MaterialButton @click="showTicket(ticket.document)" color="warning" margin="mb-0 mx-1" size="sm"
+                          icon="eye">
                           <span class="d-none d-md-block">Ver boleto</span>
                         </MaterialButton>
                       </td>
